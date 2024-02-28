7d4f8241856964d28e22e336aeb0e347c92ef751
acarolli <ayslan.carolli@outlook.com>
2024-02-28 10:27:20 -0300

    Test08

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ function renderPlaces(places) {
         let longitude = place.location.lng;
 
         let model = document.createElement('a-entity');
-        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}; clicker;`);
 
         setModel(models[modelIndex], model);
 
