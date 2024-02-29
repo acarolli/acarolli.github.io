ecd264b1a3d296ed6691ac913b51d6376736f8c4
acarolli <ayslan.carolli@outlook.com>
2024-02-29 19:00:04 -0300

    Novo

--- tesouro/script.js
+++ tesouro/script.js
@@ -62,8 +62,8 @@ function staticLoadPlaces() {
 var models = [
     {
         url: './assets/treasure/scene.gltf',
-        scale: '0.5 0.5 0.5',
-        info: 'Magnemite, Lv. 5, HP 10/10',
+        scale: '0.1 0.1 0.1',
+        info: 'Você encontrou uma pista!',
         rotation: '0 180 0',
     },
     {
