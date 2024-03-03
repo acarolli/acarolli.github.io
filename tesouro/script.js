c7d8eea5a6747872d108e9208b7ef66a949b9767
acarolli <ayslan.carolli@outlook.com>
2024-03-02 21:45:26 -0300

    Atualizar o script.js

--- tesouro/script.js
+++ tesouro/script.js
@@ -62,7 +62,7 @@ function staticLoadPlaces() {
 var models = [
     {
         url: './assets/treasure/scene.gltf',
-        scale: '0.1 0.1 0.1',
+        scale: '0.5 0.5 0.5',
         info: 'Você encontrou uma pista!',
         rotation: '0 180 0',
     },
