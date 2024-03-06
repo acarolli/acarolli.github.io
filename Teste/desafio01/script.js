1d596cc7d3a02ac2fb16a037056deb94d5a2bb76
acarolli <ayslan.carolli@outlook.com>
2024-03-06 18:20:21 -0300

    Teste 13

--- Teste/desafio01/script.js
+++ Teste/desafio01/script.js
@@ -65,13 +65,20 @@ function renderPlaces(places) {
             if (distance <= 200) {
                 topdiv.innerHTML = 'Dica Próxima!';
                 bottomdiv.innerHTML = 'Distancia: '+ distance.toFixed(2) + ' metros';
+                let element = document.getElementById("button");
+                    if (distance <= 10) {
+                        element.removeAttribute("hidden")
+                    } else {
+                        element.setAttribute("hidden", "hidden");
+                    }
+                    updateProximity(distance);
 
                 //proximityText.setAttribute('text', 'value', 'You are close to a tip');
                 //distanceText.setAttribute('text', 'value', 'Distance: ' + distance.toFixed(2) + ' meters');
             } else {
                 topdiv.innerHTML = ('');
                 bottomdiv.innerHTML = ('');
-                
+             updateProximity(distance);   
                 //proximityText.setAttribute('text', 'value', '');
                 //distanceText.setAttribute('text', 'value', '');
             }
