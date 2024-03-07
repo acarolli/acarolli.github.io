b25b168aa979d9689fb4ed56914f155b3c8eaf5e
acarolli <ayslan.carolli@outlook.com>
2024-03-06 23:56:55 -0300

    Atualizar o script.js

--- Teste/desafio01/script.js
+++ Teste/desafio01/script.js
@@ -71,8 +71,8 @@ function renderPlaces(places) {
                 //proximityText.setAttribute('text', 'value', 'You are close to a tip');
                 //distanceText.setAttribute('text', 'value', 'Distance: ' + distance.toFixed(2) + ' meters');
             } else {
-                topdiv.innerHTML = ('');
-                bottomdiv.innerHTML = ('');
+                topdiv.innerHTML = ('Nenhum dica por perto!');
+                bottomdiv.innerHTML = ('Desvende a charada.');
                 //model.setAttribute('visible',false);
                 //proximityText.setAttribute('text', 'value', '');
                 //distanceText.setAttribute('text', 'value', '');
