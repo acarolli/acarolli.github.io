17acd86758b1ae7671a2e025d579d5c621114c8d
acarolli <ayslan.carolli@outlook.com>
2024-03-11 18:52:52 -0300

    Teste 07

--- Teste/novo/script.js
+++ Teste/novo/script.js
@@ -2,6 +2,7 @@ var visibilityred = document.getElementById(red);
 var visibilitygreen = document.getElementById(green);
     
     function click() {
+            alert("Botão Clicado!");        
         if (visibilityred === "true") {
             visibilityred = "false";
             visibilitygreen = "true";
@@ -11,4 +12,12 @@ var visibilitygreen = document.getElementById(green);
             visibilitygreen = "false";
             alert("Condição 02");
         }
-    };
+    }
+
+
+// Function to open website
+        function openWebsite() {
+            // Implement your logic to open a different website for each object
+            window.open("https://www.invisio360.com", "_blank");
+            //alert('Opening website...');
+        }
