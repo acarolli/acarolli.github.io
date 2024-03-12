f90e13f02d09f03fc346aeffe3e6f01897c7edf4
acarolli <ayslan.carolli@outlook.com>
2024-03-11 21:43:53 -0300

    Teste 23

--- Teste/novo/script.js
+++ Teste/novo/script.js
@@ -1,11 +1,14 @@
     function toggleVisibility() {
       var elementoVermelho = document.getElementById('idred');
+      var elementoAzul = document.getElementById('idblue');
       var elementoVerde = document.getElementById('idgreen');
       if (elementoVermelho.getAttribute('visible')) {
         elementoVermelho.setAttribute('visible', 'false'); // Se visível, torna não visível
+        elementoAzul.setAttribute('visible', 'false'); // Se visível, torna não visível
         elementoVerde.setAttribute('visible', 'true'); // Se visível, torna não visível
       } else {
         elementoVermelho.setAttribute('visible', 'true'); // Se visível, torna não visível
+        elementoAzul.setAttribute('visible', 'true'); // Se visível, torna não visível
         elementoVerde.setAttribute('visible', 'false'); // Se visível, torna não visível
       }
     }
