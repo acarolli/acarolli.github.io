600a6154986834efd1a24f06e98a279c5c5dacc2
acarolli <ayslan.carolli@outlook.com>
2024-03-11 21:51:43 -0300

    Teste 25

--- Teste/novo/script.js
+++ Teste/novo/script.js
@@ -1,13 +1,16 @@
     function toggleVisibility() {
       var elementoVermelho = document.getElementById('idred');
       var elementoAzul = document.getElementById('idblue');
+      var elementoAmarelo = document.getElementById('idyellow');
       var elementoVerde = document.getElementById('idgreen');
       if (elementoVermelho.getAttribute('visible')) {
         elementoVermelho.setAttribute('visible', 'false'); // Se visível, torna não visível
+        elementoAmarelo.setAttribute('visible', 'false'); // Se visível, torna não visível
         elementoAzul.setAttribute('visible', 'false'); // Se visível, torna não visível
         elementoVerde.setAttribute('visible', 'true'); // Se visível, torna não visível
       } else {
         elementoVermelho.setAttribute('visible', 'true'); // Se visível, torna não visível
+        elementoAmarelo.setAttribute('visible', 'true'); // Se visível, torna não visível
         elementoAzul.setAttribute('visible', 'true'); // Se visível, torna não visível
         elementoVerde.setAttribute('visible', 'false'); // Se visível, torna não visível
       }
