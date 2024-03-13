1b8c32168c239aee54a8e84aa4d99231be6cc002
acarolli <ayslan.carolli@outlook.com>
2024-03-13 14:08:42 -0300

    Teste 01

---
+++ a/05/script.js
@@ -0,0 +1,8 @@
+export function runQuest  () {
+  // Importe os módulos necessários
+    import 'https://aframe.io/releases/1.3.0/aframe.min.js';
+    import 'https://raw.githack.com/AR-js-org/AR.js/master/three.js/build/ar-threex-location-only.js';
+    import 'https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js';
+    import 'https://cdn.jsdelivr.net/npm/aframe-animation-mixer/dist/aframe-animation-mixer.min.js';
+  // Inicialize seu conteúdo A-Frame e AR.js aqui
+}
