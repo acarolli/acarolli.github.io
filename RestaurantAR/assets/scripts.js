efca70594a2fa8408572398b73e06f6511ff55e7
acarolli <ayslan.carolli@outlook.com>
2024-08-08 11:32:58 -0300

    Teste 01

--- RestaurantAR/assets/scripts.js
+++ RestaurantAR/assets/scripts.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const categories = [
         { id: "entradas", name: "Entradas", items: [
-            { name: "Item 1", modelUrl: "modelos3d/objeto1.html" },
+            { name: "Item 1", modelUrl: "modelos3d/default.html" },
             { name: "Item 2", modelUrl: "modelos3d/objeto2.html" },
             // Continue adicionando os itens aqui
         ] },
